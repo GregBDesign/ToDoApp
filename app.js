71153dcd3eb4d2e0d30d6cf0256fe4a753742b25
Greg <80447308+GregBDesign@users.noreply.github.com>
2021-03-17 11:55:57 +1100

    Completed project

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const submit = document.querySelector('#submitBtn');
 const input = document.querySelector('input');
 const addedBtns = document.querySelector('button');
-const divContainer = document.createElement('div');
-const newLi = document.createElement('li');
-const todoList = document.querySelector('ul');
+const ulList = document.querySelector('#listItems');
+
+// Button names
 const buttonText = ['Delete', 'Done'];
 
+// Listen for a new todo
 submit.addEventListener('click', function(e){
     e.preventDefault();
     createTodo();
 })
 
+//Function for creating the todo
 createTodo = () => {
+    const divContainer = document.createElement('div');
+    const newLi = document.createElement('li');
+    const todoList = document.querySelector('ul');
     newLi.textContent = input.value;
     divContainer.append(newLi);
-    for(let i = 0 ; i < buttonText.length; i++){
-        const newButton = document.createElement('button');
+    for(let i = 0; i < buttonText.length; i++){
+        var newButton = document.createElement('button');
         newButton.textContent = buttonText[i];
         newButton.classList.add('libtn');
         divContainer.append(newButton);
@@ -24,12 +29,11 @@ createTodo = () => {
     input.value = "";
 }
 
-checkFunc = (e) => {
-    let btnType = e.target;
-    if(btnType.textContent === 'Delete'){
-        this.newLi.remove();
-        //Check udemy vid on .this??
+//Listen for the button actions
+ulList.addEventListener('click', function(e){
+    if(e.target.textContent === 'Delete'){
+        e.target.parentNode.remove();
+    } else {
+        e.target.parentNode.classList.add('done');
     }
-}
-
-document.addEventListener('click', checkFunc);
+})
